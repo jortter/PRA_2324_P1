29f243ef9fda8ae02736b76843062c9f9edf6189
jorter <jortter@etsinf.upv.es>
2024-10-03 19:16:18 +0200

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -8,15 +8,37 @@
 template <typename T> 
 class List {
     public:       
-	 // ... aquí los métodos virtuales puros
-	void insert(int pos, T e);	
-	void append(T e);
-	void prepend(T e);
-	T remove(int pos);
-	T get(int pos);
-	int search(T e);
-	bool empty();
-	int size();
+	// Metodos virtuales
+	// Inserta el elemento e en la posición pos
+        virtual void insert(int pos, T e) = 0;
+
+        // Inserta el elemento e al final de la lista
+        virtual void append(T e) {
+            insert(size(), e);  // append es equivalente a insertar al final
+        }
+
+        // Inserta el elemento e al principio de la lista
+        virtual void prepend(T e) {
+            insert(0, e);  // prepend es equivalente a insertar al inicio
+        }
+
+        // Elimina y devuelve el elemento en la posición pos
+        virtual T remove(int pos) = 0;
+
+        // Devuelve el elemento en la posición pos
+        virtual T get(int pos) const = 0;
+
+        // Busca la primera ocurrencia de e y devuelve su posición, o -1 si no está
+        virtual int search(T e) const = 0;
+
+        // Indica si la lista está vacía
+        virtual bool empty() const = 0;
+
+        // Devuelve el número de elementos en la lista
+        virtual int size() const = 0;
+
+        // Destructor virtual para garantizar la correcta destrucción de las subclases
+        virtual ~List() {}
 };
 
 #endif
